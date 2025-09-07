af4107893c60af25dea001d56b5aaa25d8e91c27
Denis Miri <denismiri1234@gmail.com>
2025-09-07 16:45:32 +0200

    added error handler

--- httpServer/main.go
+++ httpServer/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"Denis.test/inernal/requests"
+	"Denis.test/inernal/responce"
 	"Denis.test/inernal/server"
 )
 
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, func(w io.Writer, req *requests.Request) *server.HandlerError{
+		if req.RequestLine.RequestTarget == "/yourproblem" {
+
+			return &server.HandlerError{
+				StatusCode: responce.BAD_REQUEST,
+				Message: "Your problem is not my problem\n",
+			}
+		} else if req.RequestLine.RequestTarget == "/myproblem" {
+
+			return &server.HandlerError{
+				StatusCode: responce.INTERNAL_SERVER_ERROR,
+				Message: "Woopsie, my bad\n",
+			}
+		} else {
+			w.Write([]byte("All good, frfr\n"))
+		}
+		return nil
+	})
+
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
--- inernal/server/server.go
+++ inernal/server/server.go
@@ -1,29 +1,56 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net"
 
+	"Denis.test/inernal/requests"
 	"Denis.test/inernal/responce"
 )
 
 type Server struct {
 	closed bool
+	handler Handler
 }
 
-func runConnection(_s *Server,conn io.ReadWriteCloser){
+type HandlerError struct{
+	StatusCode  responce.StatusCode
+	Message 	string
+}
+
+func runConnection(s *Server,conn io.ReadWriteCloser){
 	defer conn.Close()
 	
-	buf := make([]byte, 4096) 
-    _, err := conn.Read(buf)
-    if err != nil && err != io.EOF {
-        return
-    }
 
 	header := responce.GetDefaultHeaders(0)
-	responce.WriteStatusLine(conn , responce.OK)
+	r ,err := requests.ReqFromReader(conn)
+	if err !=nil {
+		responce.WriteStatusLine(conn , responce.BAD_REQUEST)
+		responce.WriteHeaders(conn , header)
+		return
+	}
+
+	writer := bytes.NewBuffer([]byte{})
+	errhandle := s.handler(writer, r)
+
+	var body []byte = nil
+	var sat responce.StatusCode = responce.OK
+
+	if errhandle != nil {
+		sat = errhandle.StatusCode
+		body = []byte(errhandle.Message)
+	} else {
+		body = writer.Bytes()
+			
+	}
+
+	header.Replace("Content-Length", fmt.Sprintf("%d",len(body)))
+
+	responce.WriteStatusLine(conn , sat)
 	responce.WriteHeaders(conn , header)
+	conn.Write(body)
 }
 
 func runServer(s *Server, listener net.Listener)  {
@@ -42,13 +69,17 @@ func runServer(s *Server, listener net.Listener)  {
 	}
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int , handler Handler) (*Server, error) {
 	lisener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return nil ,err 
 	}
 
-	serve := &Server{ closed: false }
+	serve := &Server{ 
+		closed: false,
+		handler: handler,
+	}
+
 	go runServer(serve, lisener)
 
 	return serve, nil
@@ -58,3 +89,5 @@ func (s *Server) Close() error{
 	s.closed = true
 	return nil
 }
+
+type Handler func(w io.Writer, req *requests.Request) *HandlerError
